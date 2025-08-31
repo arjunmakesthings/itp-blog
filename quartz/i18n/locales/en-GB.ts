ecfe5ce220594adc235b912871c3a6c07d7681a0
arjun <82059571+arjunmakesthings@users.noreply.github.com>
2025-08-31 14:29:44 -0400

    change phrasing — items vs notes.

--- quartz/i18n/locales/en-GB.ts
+++ quartz/i18n/locales/en-GB.ts
@@ -73,7 +73,7 @@ export default {
     folderContent: {
       folder: "Folder",
       itemsUnderFolder: ({ count }) =>
-        count === 1 ? "1 item under this folder." : `${count} items under this folder.`,
+        count === 1 ? "1 note in this folder." : `${count} notes in this folder.`,
     },
     tagContent: {
       tag: "Tag",
