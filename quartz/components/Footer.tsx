528ffe718828d6f4c397e1fda40c1f3c5f17ee31
arjun <82059571+arjunmakesthings@users.noreply.github.com>
2025-08-31 14:08:23 -0400

    reformat.

--- quartz/components/Footer.tsx
+++ quartz/components/Footer.tsx
@@ -15,7 +15,11 @@ export default ((opts?: Options) => {
       <footer class={`${displayClass ?? ""}`}>
         <p>
           {/* {i18n(cfg.locale).components.footer.createdWith}{" "} */}
-          made by <a href="https://arjunmakesthings.github.io" target = "_blank">arjun</a>; using: <a href="https://quartz.jzhao.xyz/">quartz v{version}</a> © {year}.
+          made by{" "}
+          <a href="https://arjunmakesthings.github.io" target="_blank">
+            arjun
+          </a>
+          ; using: <a href="https://quartz.jzhao.xyz/">quartz v{version}</a> © {year}.
         </p>
         <ul>
           {Object.entries(links).map(([text, link]) => (
